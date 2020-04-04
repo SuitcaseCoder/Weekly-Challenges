be94a6b05dc25c38abc66c5e3ad4aa29534f36dd
Laura Ruiz-Roehrs <lauraruizroehrs@lauras-mbp.lan>
2020-04-04 17:52:21 -0500

    ep4 without notes

---
+++ ep_04/practice.js
@@ -0,0 +1,32 @@
+//Given a string, find the longest subsequence consisting of a single character.  Example: longest("ABAACDDDBBA") should return  D
+
+function consecutive(str)
+{
+  var arr = str.split('');
+  var object = {};
+  var pChar = '';
+  var baseCount = 0;
+  var maxCount = 0;
+
+  for(var i=0; i<=arr.length; i++)
+  {
+    if(baseCount > maxCount)
+    {
+      maxCount = baseCount;
+      object = {};
+      object[pChar] = maxCount;
+    }
+
+    var char = arr[i];
+    if(pChar !== '' && char != pChar)
+    {
+      baseCount = 0;
+    }
+    baseCount++;
+    pChar = char;
+  }
+  return object;
+}
+
+
+console.log(consecutive("AABCDDBBBEEEEA"))
